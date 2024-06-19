c8c62d31b3d03aabd2d5e7533a20ae69e441e7c5
kyber03 <102473674+kyber03@users.noreply.github.com>
2024-06-19 23:01:49 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import react from "react";
+import { useState } from "react";
+import Board from "./board";
+import Colorgame from "./colorgame";
+import Bmicalc from "./bmi";
+
+export default function App() {
+  return (
+    <div>
+      <Bmicalc />
+    </div>
+  );
+}
